8bb2a7d1c9de39a7d4d782c734f326832a6dc945
Danilo Curvelo <danilocurvelo@gmail.com>
2023-11-09 15:26:18 -0300

    Update Owned.sol

--- code/solidity/Owned.sol
+++ code/solidity/Owned.sol
@@ -1,1 +1,22 @@
+pragma solidity ^ 0.5.0;
 
+contract Owned {
+    
+    address payable owner;
+    
+    constructor() public {
+        owner = msg.sender;
+    }
+    
+    modifier onlyOwner {
+        require(msg.sender == owner, "Somente o dono do contrato pode executar essa funcao!");
+        _;
+    }
+    
+}
+
+contract Mortal is Owned {
+    function destroy() public onlyOwner {
+        selfdestruct(owner);
+    }
+}
