4b10fc695b7cef4a987ee9a75601dd70b82f3cce
Danilo Curvelo <danilocurvelo@gmail.com>
2023-11-09 15:24:53 -0300

    Update Coin.sol

--- code/solidity/Coin.sol
+++ code/solidity/Coin.sol
@@ -1,1 +1,27 @@
+pragma solidity ^0.7.0;
 
+contract Coin {
+    
+    address public minter; // dono do contrato
+    mapping (address => uint) public balances;
+    
+    event Transferencia(address from, address to, uint amount);
+    
+    constructor() {
+        minter = msg.sender;
+    }
+    
+    function mint(address receiver, uint amount) public {
+        require(msg.sender == minter);
+        balances[receiver] += amount;
+    }
+    
+    function send(address receiver, uint amount) public {
+        require(amount <= balances[msg.sender], "Saldo insuficiente.");
+        
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+        emit Transferencia(msg.sender, receiver, amount);
+    }
+    
+}
